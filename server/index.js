6139328755df7d55e885cbe10a5e5054347979ef
anirudh-singh01 <anirudhsingh.ani321@gmail.com>
2024-09-17 12:23:00 +0530

    adding flies (Backend)

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser"); // Add cookie-parser
+const crypto = require("crypto");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET)); // Add signed cookies
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
